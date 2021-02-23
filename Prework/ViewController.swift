f0be31469c1a7348db0117880ba3fee686904147
yuan106 <61606121+yuan106@users.noreply.github.com>
2021-02-23 13:32:51 -0800

    Initial Commit

---
+++ Prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by SU YUAN on 2/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
